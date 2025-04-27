e4b2b1fdcd88ae1d11a8a49898503866dadf55f4
aaban-khan <aabankhan.404@gmail.com>
2025-04-28 00:04:38 +0530

    Updateing Logic

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 import random 
 
 items_list = ["Rock", "Paper", "Scissor"]
-user = input("Enter your Move(Rock, Paper, Scissor): ").capitalize()
-comp = random.choice(items_list)
 
-print(f"\nFinal Result")
-print(f"👤 User's Move: {user}")
-print(f"💻 Computer's Move: {comp}")
+def game_logic():
+    user = input("\nEnter your Move(Rock, Paper, Scissor): ").capitalize()
+    comp = random.choice(items_list)
 
-if user == comp:
-    print("=>😅 Both Chooses Same: Match Tie")
+    if user not in items_list:
+        print(f"❌ Invalid Move! Please choose Rock, Paper, or Scissor.")
 
-elif user == "Rock":
-    if comp == "Paper":
-        print("=>🥲 Paper covers Rock: Computer Win")
     else:
-        print("=>😁 Rock smashes Scissor: User Win")
+        print(f"\nFinal Result: ")
+        print(f"👤 User's Move: {user}")
+        print(f"💻 Computer's Move: {comp}")
 
-elif user == "Paper":
-    if comp == "Rock":
-        print("=>😁 Paper covers Rock: User Win")
-    else:
-        print("=>🥲 Scissor cuts Paper: Computer Win")
+        if user == comp:
+            print("=>😅 Both Chooses Same: Match Tie")
 
-elif user == "Scissor":
-    if comp == "Paper":
-        print("=>😁 Scissor cuts Paper: You Win")
-    else:
-        print("=>🥲 Rock smashes Scissor: Computer Win")
+        elif user == "Rock":
+            if comp == "Paper":
+                print("=>🥲 Paper covers Rock: Computer Win")
+            else:
+                print("=>😁 Rock smashes Scissor: User Win")
+
+        elif user == "Paper":
+            if comp == "Rock":
+                print("=>😁 Paper covers Rock: User Win")
+            else:
+                print("=>🥲 Scissor cuts Paper: Computer Win")
+
+        elif user == "Scissor":
+            if comp == "Paper":
+                print("=>😁 Scissor cuts Paper: You Win")
+            else:
+                print("=>🥲 Rock smashes Scissor: Computer Win")
+
+while True: 
+    game_logic()
+    play_again = input("\n=> Do you want to Play again(y/n): ").lower()
+    if play_again != "y":
+        print(f"Thanks for Playing 👏")
+        break
